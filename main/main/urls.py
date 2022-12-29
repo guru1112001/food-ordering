96334731b4d7f2018a388d092081b0993557b250
guru1112001 <107809909+guru1112001@users.noreply.github.com>
2022-12-29 17:54:31 +0530

    create the dashboard

--- main/main/urls.py
+++ main/main/urls.py
@@ -1,6 +1,6 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from account import views
 from django.contrib.auth import views as auth_views
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path("register/",views.RegisterPage,name="register"),
     path("login/",auth_views.LoginView.as_view(template_name="account/login.html"),name="login"),
     path("logout/",auth_views.LogoutView.as_view(template_name="account/logout.html"),name="logout"),
+    path("",include("ordering.urls")),
 ]
--- main/ordering/views.py
+++ main/ordering/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .models import cart,order_info,Product
+from account.models import Customer
 # Create your views here.
+def Dashboard(request):
+    orders_info=order_info.objects.all()
+    carts=cart.objects.all()
+    products=Product.objects.all()
+    customers=Customer.objects.all()
+
+    total_customer=customers.count()
+    total_order=orders_info.count()
+    take_away=orders_info.filter(take_away="Yes").count()
+    payment=orders_info.filter(complete="True").count()
+
+    context={"orders_info":orders_info, "carts":carts, "products":products , "customers":customers , "total_customer":total_customer, "total_order":total_order , "take_away":take_away , "payment":payment }
+    
+    return render(request,"ordering/dashboard.html",context)
+
+
+
